bdbd6d091fa40ef036e3d27b0209c72694441015
utt6161 <utt6161@yandex.ru>
2021-12-19 20:58:10 +0100

    selection timeout adjustment

--- src/components/MobileList/Accordion.tsx
+++ src/components/MobileList/Accordion.tsx
@@ -17,7 +17,7 @@ export const Accordion = (props: AccordionProps) => {
     const dispatch = useDispatch();
     const longPress = useLongPress(() => {
         dispatch(switcher(props.id))
-    }, 200)
+    }, 350)
     return (
         <div className="box-content border-t border-gray-300">
             <div className="">
