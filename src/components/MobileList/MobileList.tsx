f995e5c6e56e31fc3339398e50e659c5dfd1f574
utt6161 <utt6161@yandex.ru>
2021-12-21 05:31:22 +0100

    last minute fix: issue with components having not so smart ids which caused them having the same innner state

--- src/components/MobileList/MobileList.tsx
+++ src/components/MobileList/MobileList.tsx
@@ -7,7 +7,9 @@ import {selectCurrentPage} from "../../store/companySlice";
 const MobileList = (props: DataProps) => {
     const currentPage = useSelector(selectCurrentPage)
     const mobileTableData = props.items.map((value,number ) => {
-        return <MobileListItem key = {(currentPage * 10) + number} data={value}/>
+        // adding id to that multiplication seems obsolete, but i dont remember why
+        // i did that in a first place, so i wont touch it, just to be sure
+        return <MobileListItem key = {(currentPage * 10) + value.id} data={value}/>
     })
     return (
 
